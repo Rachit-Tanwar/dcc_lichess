01d7da6978bdda1711361a1ec1710f880bf45c23
Rachit-Tanwar <rachittanwar7124@gmail.com>
2023-06-14 20:01:47 +0530

    first test

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 # Create .env file with API key
 
 import os
+import json
 import berserk
 import sqlite3
 from dotenv import load_dotenv
 from requests_oauthlib import OAuth2Session
 
+
+con = sqlite3.connect("dcc.db")
+cursor = con.cursor()
+
+#cursor.execute('''CREATE TABLE IF NOT EXISTS Tournaments(
+#                  USERNAME TEXT(34),
+#                  POINTS INT(),
+#                  TOURNAMENTS PLAYED INT(),
+#                  TOURNAMENTS WON INT(),
+#                  MATCHES PLAYED INT(),
+#                  WON INT(),
+#                  LOST INT(),
+#                  DRAW INT(),
+#                  BYES INT(),
+#
+#)''')
+
+
 load_dotenv(override=True) 
 
 API_TOKEN = os.getenv("API_KEY")
-session = OAuth2Session(API_TOKEN)
-client = berserk.Client(session)
+session = berserk.TokenSession(API_TOKEN) #type:ignore 
+client = berserk.Client(session=session)
 
+mem = client.users.get_all_top_10()
+list(mem)
+print(mem['bullet'][0])
